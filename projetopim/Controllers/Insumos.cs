00dc68500c2df5d24df2494c7e4b03d51d1b5280
Augusto Santos <gutozupcraft@gmail.com>
2024-11-08 23:08:20 -0300

    Adicionar arquivos de projeto.

---
+++ projetopim/Controllers/Insumos.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace projetopim.Controllers
+{
+    public class Insumos : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+    }
+}
---
+++ projetopim/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using projetopim.Models;
+using System.Diagnostics;
+
+namespace projetopim.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View("Login");
+        }
+
+        [HttpPost("login")]
+        public IActionResult Authenticate(string username, string password)
+        {
+            // Autenticação simplificada (apenas exemplo)
+            if (username == "admin" && password == "password")
+            {
+                // Redireciona para uma página principal após o login bem-sucedido
+                return RedirectToAction("Index", "Home");
+            }
+
+            // Em caso de falha, retorna à tela de login com uma mensagem de erro
+            ViewBag.ErrorMessage = "Nome de usuário ou senha incorretos.";
+            return View("Login");
+        }
+    }
+}
